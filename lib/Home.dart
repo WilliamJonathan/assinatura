59e609bdeef4ba1e74fa7379b77b8a6a947ba3cc
Jonathan <jonathanwilliamfirebase@gmail.com>
2020-06-12 17:17:42 -0300

    Jonathan William

--- lib/Home.dart
+++ lib/Home.dart
@@ -23,72 +23,68 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Builder(
-        builder: (context) => Scaffold(
-          body: ListView(
-            children: <Widget>[
-              Container(
-                height: 300,
-                child: Center(
-                  child: Text('Container grande para testar problemas de rolagem'),
-                ),
-              ),
-              //TELA DE ASSINATURA
-              Signature(
-                controller: _controller,
-                height: 300,
-                backgroundColor: Colors.lightBlueAccent,
-              ),
-              //BOTÕES APROVADO E APAGAR
-              Container(
-                decoration: const BoxDecoration(color: Colors.black),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    //MOSTRAR IMAGEM EXPORTADA EM NOVA ROTA
-                    IconButton(
-                      icon: const Icon(Icons.check),
-                      color: Colors.blue,
-                      onPressed: () async {
-                        if (_controller.isNotEmpty) {
-                          var data = await _controller.toPngBytes();
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (BuildContext context) {
-                                return Scaffold(
-                                  appBar: AppBar(),
-                                  body: Center(
-                                      child: Container(
-                                          color: Colors.grey[300], child: Image.memory(data))),
-                                );
-                              },
-                            ),
-                          );
-                        }
-                      },
-                    ),
-                    //LIMPAR TELA
-                    IconButton(
-                      icon: const Icon(Icons.clear),
-                      color: Colors.blue,
-                      onPressed: () {
-                        setState(() => _controller.clear());
-                      },
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                height: 300,
-                child: Center(
-                  child: Text('Container grande para testar problemas de rolagem'),
-                ),
-              ),
-            ],
+    return Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 300,
+            child: Center(
+              child: Text('Container grande para testar problemas de rolagem'),
+            ),
           ),
-        ),
+          //TELA DE ASSINATURA
+          Signature(
+            controller: _controller,
+            height: 300,
+            backgroundColor: Colors.lightBlueAccent,
+          ),
+          //BOTÕES APROVADO E APAGAR
+          Container(
+            decoration: const BoxDecoration(color: Colors.black),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                //MOSTRAR IMAGEM EXPORTADA EM NOVA ROTA
+                IconButton(
+                  icon: const Icon(Icons.check),
+                  color: Colors.blue,
+                  onPressed: () async {
+                    if (_controller.isNotEmpty) {
+                      var data = await _controller.toPngBytes();
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (BuildContext context) {
+                            return Scaffold(
+                              appBar: AppBar(),
+                              body: Center(
+                                  child: Container(
+                                      color: Colors.green[300], child: Image.memory(data))),
+                            );
+                          },
+                        ),
+                      );
+                    }
+                  },
+                ),
+                //LIMPAR TELA
+                IconButton(
+                  icon: const Icon(Icons.clear),
+                  color: Colors.blue,
+                  onPressed: () {
+                    setState(() => _controller.clear());
+                  },
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 300,
+            child: Center(
+              child: Text('Container grande para testar problemas de rolagem'),
+            ),
+          ),
+        ],
       ),
     );
   }
